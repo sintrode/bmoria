d06cc8c756465a9042feae621ed40e6e554f288c
sintrode <562c3fb8@opayq.com>
2023-02-08 02:44:56 -0500

    Continued inventory subroutines

--- src/sys/inventory.cmd
+++ src/sys/inventory.cmd
@@ -109,7 +109,7 @@ if %~1 GEQ %PlayerEquipment.Wield% (
         set /a py.inventory[%~1].items_count-=1
     )
 
-    call identfication.cmd :itemDescription prt1 "game.treasure.list[!treasure_id!]" "true"
+    call identification.cmd :itemDescription prt1 "game.treasure.list[!treasure_id!]" "true"
     call ui_io.cmd :printMessage "Dropped !prt1!"
 )
 set /a "py.flags.status|=%config.player.status.py_str_wgt%"
@@ -138,40 +138,359 @@ for /L %%A in (0,1,!counter_dec!) do (
 )
 exit /b !damage!
 
+::------------------------------------------------------------------------------
+:: Reduces the amount of light available by the light-providing item
+::
+:: Arguments: %1 - Indicates if the player noticed that the light dimmed or not
+:: Returns:   0 if the player sees that the light dimmed
+::            1 if the player did not see the light dim
+::------------------------------------------------------------------------------
 :inventoryDiminishLightAttack
-exit /b
+set "noticed=%~1"
+if !py.inventory[%PlayerEquipment.Light%]! GTR 0 (
+    call rng.cmd :randomNumber 250
+    set /a py.inventory[%PlayerEquipment.Light%].misc_use-=250+!errorlevel!
 
+    if !py.inventory[%PlayerEquipment.Light%].misc_use! LSS 1 (
+        set "py.inventory[%PlayerEquipment.Light%].misc_use=1"
+    )
+
+    if %py.flags.blind% LSS 1 (
+        call ui_io.cmd :printMessage "Your light dims."
+    ) else (
+        set "noticed=false"
+    )
+) else (
+    set "noticed=false"
+)
+
+:: ah, the pains of using a string to simulate a boolean...
+if "!noticed!"=="true" exit /b 0
+exit /b 1
+
+::------------------------------------------------------------------------------
+:: Reduces the number of available charges and increases a monster's HP
+::
+:: Arguments: %1 - The level of the creature draining the item's energy
+::            %2 - A reference to the monster's HP
+::            %3 - Determines if the player noticed energy being taken
+:: Returns:   0 if the player gets informed about the drain
+::            1 if the drain happens silently
+::------------------------------------------------------------------------------
 :inventoryDiminishChargesAttack
-exit /b
+call rng.cmd :randomNumber %py.pack.unique_items%
+set /a rnd_item=!errorlevel!-1
+set "item=py.inventory[%rnd_item%]"
 
+set "has_charges=false"
+if "!%item%.category_id!"=="%tv_staff%" set "has_charges=true"
+if "!%item%.category_id!"=="%tv_wand%"  set "has_charges=true"
+
+if "%has_charges%"=="true" (
+    if !%item%.misc_use! GTR 0 (
+        set /a !%~2!+=%~1*!%item%.misc_use!
+        set "%item%.misc_use=0"
+
+        call identification.cmd :spellItemIdentified "%item%" || (
+            call identification.cmd :itemAppendToInscription "%item%" "%config.identification.id_empty%"
+        )
+        call ui_io.cmd :printMessage "Energy drains from your pack."
+    ) else (
+        set "noticed=false"
+    )
+)
+if "!noticed!"=="true" exit /b 0
+exit /b 1
+
+::------------------------------------------------------------------------------
+:: Removes the enchantment from a random wielded item
+::
+:: Arguments: None
+:: Returns:   0 if to_hit, to_damage, or to_ac is reduced
+::            1 if there is nothing to reduce
+::------------------------------------------------------------------------------
 :executeDisenchantAttack
-exit /b
+call rng.cmd :randomNumber 7
+set "rnd_item=!errorlevel!"
 
-:inventoryCanCarryItemCount
-exit /b
+if "!rnd_item!"=="1" set "item_id=%PlayerEquipment.Wield%"
+if "!rnd_item!"=="2" set "item_id=%PlayerEquipment.Body%"
+if "!rnd_item!"=="3" set "item_id=%PlayerEquipment.Arm%"
+if "!rnd_item!"=="4" set "item_id=%PlayerEquipment.Outer%"
+if "!rnd_item!"=="5" set "item_id=%PlayerEquipment.Hands%"
+if "!rnd_item!"=="6" set "item_id=%PlayerEquipment.Head%"
+if "!rnd_item!"=="7" set "item_id=%PlayerEquipment.Feet%"
 
+set "success=1"
+set "item=py.inventory[%item_id%]"
+
+for %%A in (to_hit to_damage to_ac) do (
+    if !%item%.%%A! GTR 0 (
+        call rng.cmd :randomNumber 2
+        set /a %item%.%%A-=!errorlevel!
+        if !%item%.%%A! LSS 0 set "%item%.%%A=0"
+        set "success=0"
+    )
+)
+exit /b !success!
+
+::------------------------------------------------------------------------------
+:: Determine if picking up an object would change the player's speed
+::
+:: Arguments: %1 - A reference to the item to pick up
+:: Returns:   0 if this item makes the pack too heavy to move
+::            1 if this item can be picked up without effort
+::------------------------------------------------------------------------------
 :inventoryCanCarryItem
-exit /b
+call player.cmd :playerCarryingLoadLimit
+set "limit=!errorlevel!"
+set /a new_weight=!%~1.items_count!*!%~1.weight!+%py.pack.weight%
 
+if !limit! LSS !new_weight! (
+    set /a "limit=!new_weight!/(!limit!+1)"
+) else (
+    set "limit=0"
+)
+
+if "%py.pack.heaviness%"=="!limit!" exit /b 0
+exit /b 1
+
+::------------------------------------------------------------------------------
+:: Tries to pick up an item and stack it in the inventory. The C code insists
+:: that this code needs to be identical to :inventoryCarryItem for some reason.
+:: This will likely get purged during the second pass rewrite.
+::
+:: Arguments: %1 - A reference to the item being picked up
+:: Returns:   0 if the item being picked up is stacked with others like it
+::            1 if the item is different from the rest of the inventory
+::------------------------------------------------------------------------------
+:inventoryCanCarryItemCount
+if %py.pack.unique_items% LSS %PlayerEquipment.Wield% exit /b 0
+call :inventoryItemStackable %1 || exit /b 1
+
+set /a counter_dec=%py.pack.unique_items%-1
+for /L %%A in (0,1,!counter_dec!) do (
+    set "same_character=false"
+    set "same_category=false"
+    set "same_number=false"
+    set "same_group=false"
+    set "identification=false"
+
+    if "!py.inventory[%%A].category_id!"=="!%~1.category_id!" set "same_character=true"
+    if "!py.inventory[%%A].sub_category_id!"=="!%~1.sub_category_id!" set "same_category=true"
+    
+    set /a new_item_total=!py.inventory[%%A].items_count!+!%~1.items_count!
+    if !new_item_total! LSS 256 set "same_number=true"
+
+    call identification.cmd :itemSetColorlessAsIdentified !py.inventory[%%A].category_id! !py.inventory[%%A].sub_category_id! !py.inventory[%%A].identification!
+    set "inventory_item_is_colorless=!errorlevel!"
+    call identification.cmd :itemSetColorlessAsIdentified !%~1.category_id! !%~1.sub_category_id! !%~1.identification!
+    set "item_is_colorless=!errorlevel!"
+    if "!inventory_item_is_colorless!"=="!item_is_colorless!" set "identification=true"
+
+    if "!same_character!"=="true" (
+        if "!same_category!"=="true" (
+            if "!same_number!"=="true" (
+                if "!same_group!"=="true" (
+                    if "!identification!"=="true" (
+                        exit /b 0
+                    )
+                )
+            )
+        )
+    )
+)
+exit /b 1
+
+::------------------------------------------------------------------------------
+:: Picks up an item and adds it to the inventory, returning the item position
+:: for a description if needed.  The C code insists that this code needs to be
+:: identical to :inventoryCarryItemCount for some reason.
+::
+:: Arguments: %1 - A reference to the item being picked up
+:: Returns:   The index of py.inventory that the item is stored in
+::------------------------------------------------------------------------------
 :inventoryCarryItem
-exit /b
+call identification.cmd :itemSetColorlessAsIdentified !%~1.category_id! !%~1.sub_category_id! !%~1.identification!
+set "is_known=!errorlevel!"
+set "is_always_known=false"
+call identification.cmd :objectPositionOffset !%~1.category_id! !%~1.sub_category_id!
+if "!errorlevel!"=="-1" set "is_always_known=true"
 
+set "is_same_category=false"
+set "is_same_sub_category=false"
+set "not_too_many_items=false"
+set "same_known_status=false"
+set "is_stackable=false"
+set "is_same_group=false"
+
+set /a counter_dec=%player_inventory_size%-1
+for /L %%A in (0,1,!counter_dec!) do (
+    set "slot_id=%%A"
+    if "!%~1.category_id!"=="!py.inventory[%%A].category_id!" set "is_same_category=true"
+    if "!%~1.sub_category_id!"=="!py.inventory[%%A].sub_category_id!" set "is_same_sub_category=true"
+    set /a new_item_total=!%~1.items_count!+!py.inventory[%%A].items_count!
+    if !new_item_total! LSS 256 set "not_too_many_items=true"
+
+    call identification.cmd :itemSetColorlessAsIdentified !py.inventory[%%A].category_id! !py.inventory[%%A].sub_category_id! !py.inventory[%%A].identification!
+    call :inventoryItemStackable %1 && set "is_stackable=true"
+    if "!is_known!"=="!errorlevel!" set "same_known_status=true"
+    if !%~1.sub_category_id! LSS %item_group_min% set "is_same_group=true"
+    if "!%~1.misc_use!"=="!py.inventory[%%A].misc_use!" set "is_same_group=true"
+
+    set "add_item=0"
+    for %%B in (is_same_category is_same_sub_category is_stackable not_too_many_items is_same_group same_known_status) do (
+        if "!%%B!"=="true" set /a add_item+=1
+    )
+    if "!add_item!"=="6" (
+        set /a py.inventory[%%A].items_count+=!%~1.items_count!
+        goto :inventoryCarryItemAfterLoop
+    )
+
+    set "add_item=0"
+    if "!is_same_category!"=="true" (
+        if !%~1.sub_category_id! LSS !py.inventory[%%A].sub_category_id! (
+            if "!is_always_known!"=="true" (
+                set "add_item=1"
+            )
+        )
+    )
+    if !%~1.category_id! GTR !py.inventory[%%A].category_id! set "add_item=1"
+
+    if "!add_item!"=="1" (
+        set /a counter_dec=%py.pack.unique_items%-1
+        for /L %%B in (!counter_dec!,-1,%%A) do (
+            set /a counter_inc=%%B+1
+            call :inventoryCopyItem "py.inventory[!counter_inc!]" "py.inventory[%%B]"
+        )
+        call :inventoryCopyItem "py.inventory[%%A]" "%~1"
+        set /a py.pack.unique_items+=1
+        goto :inventoryCarryItemAfterLoop
+    )
+)
+
+:inventoryCarryItemAfterLoop
+set /a py.pack.weight+=!%~1.items_count!*!%~1.weight!
+set /a "py.flags.status|=%config.player.status.py.str_wgt%"
+exit /b !slot_id!
+
+::------------------------------------------------------------------------------
+:: Sets the start and end indices for a range of items in the inventory
+::
+:: Arguments: %1 - The start of the range to search
+::            %2 - The end of the range to search
+::            %3 - A reference to the start of the found range
+::            %4 - A reference to the end of the found range
+:: Returns:   0 if there is an item at either end of the given range
+::            1 if the item is not in the given range
+::------------------------------------------------------------------------------
 :inventoryFindRange
-exit /b
+set "%~3=-1"
+set "%~4=-1"
 
+set "at_end_of_range=false"
+set /a counter_dec=%py.pack.unique_items%-1
+for /L %%A in (0,1,!counter_dec!) do (
+    set "item_id=!py.inventory[%%A].category_id!"
+
+    if "!at_end_of_range!"=="false" (
+        if "!item_id!"=="%~1" (
+            set "at_end_of_range=true"
+            set "%~3=%%A"
+        )
+        if "!item_id!"=="%~2" (
+            set "at_end_of_range=true"
+            set "%~3=%%A"
+        )
+    ) else (
+        if not "!item_id!"=="%~1" (
+            if not "!item_id!"=="%~2" (
+                set /a %~4=%%A-1
+                goto :inventoryFindRangeAfterLoop
+            )
+        )
+    )
+)
+
+:inventoryFindRangeAfterLoop
+if "!at_end_of_range!"=="true" (
+    if "!%~4!"=="-1" (
+        set k=!counter_dec!
+    )
+)
+
+if "!at_end_of_range!"=="true" exit /b 0
+exit /b 1
+
+::------------------------------------------------------------------------------
+:: Copies an item from the game_objects array to the inventory
+::
+:: Arguments: %1 - The ID of the item to copy
+::            %2 - A reference to the item being copied to
+:: Returns:   None
+::------------------------------------------------------------------------------
 :inventoryItemCopyTo
+set "from=game_objects[%~1]"
+
+set "%~2.id=%~1"
+set "%~2.special_name_id=%SpecialNameIds.sn_null%"
+set "%~2.inscription="
+for %%A in (category_id sprite misc_use cost sub_category_id items_count
+            weight to_hit to_damage ac to_ac "damage.dice" "damage.sides"
+            depth_first_found) do (
+    set "%~2.%%A=!%game%.%%A!"
+)
+set "%~2.identification=0"
 exit /b
 
+::------------------------------------------------------------------------------
+:: Checks if a single item is stackable
+::
+:: Arguments: %1 - A reference to the item being checked
+:: Returns:   0 if the item can be stacked
+::            1 if the item can not be stacked
+::------------------------------------------------------------------------------
 :inventoryItemSingleStackable
-exit /b
+if !%~1.sub_category_id! GEQ %item_single_stack_min% (
+    if !%~1.sub_category_id! LEQ %item_single_stack_max% (
+        exit /b 0
+    )
+)
+exit /b 1
 
+::------------------------------------------------------------------------------
+:: Checks if an item or group of items is stackable
+::
+:: Arguments: %1 - A reference to the item being checked
+:: Returns:   0 if the item can be stacked
+::            1 if the item can not be stacked
+::------------------------------------------------------------------------------
 :inventoryItemStackable
-exit /b
+if !%~1.sub_category_id! GEQ %item_single_stack_min% (
+    exit /b 0
+)
+exit /b 1
 
+::------------------------------------------------------------------------------
+:: Checks if an item is cursed
+::
+:: Arguments: %1 - A reference to the item being checked
+:: Returns:   0 if the item is cursed
+::            1 if the item is not cursed
+::------------------------------------------------------------------------------
 :inventoryItemIsCursed
-exit /b
+set /a "is_cursed=!%~1.flags! & %config.treasure.flags.tr_cursed%"
+if "!is_cursed!"=="0" exit /b 1
+exit /b 0
 
+::------------------------------------------------------------------------------
+:: Removes any curse on a specified item
+::
+:: Arguments: %1 - A reference to the item being decursed
+:: Returns:   None
+::------------------------------------------------------------------------------
 :inventoryItemRemoveCurse
+set /a "%~1.flags&=~%config.treasure.flags.tr_cursed%"
 exit /b
 
 :damageMinusAC
@@ -275,7 +594,7 @@ exit /b
 :inventoryCopyItem
 for %%T in (id special_name_id inscription flags category_id sprite misc_use
             cost sub_category_id items_count weight to_hit to_damage ac to_ac
-            damage.dice damage.sides depth_first_round identification) do (
+            damage.dice damage.sides depth_first_found identification) do (
     set "%~1.%%T=!%~2.%%T"
 )
 exit /b
