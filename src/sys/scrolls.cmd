528f2c092c57fdd4e0e22f515319c2d44880d90c
sintrode <562c3fb8@opayq.com>
2023-02-28 17:10:36 -0500

    Checked to see if the player can read a scroll

--- src/sys/scrolls.cmd
+++ src/sys/scrolls.cmd
@@ -2,8 +2,40 @@
 call %*
 exit /b
 
+::------------------------------------------------------------------------------
+:: Determines if a player is able to read the scroll at all
+::
+:: Arguments: %1 - The variable that the starting range of the scrolls is in
+::            %2 - The variable that the ending range of the scrolls is in
+:: Returns:   0 if the player is able to read a scroll
+::            1 if there is some factor preventing the player from reading
+::------------------------------------------------------------------------------
 :playerCanReadScroll
-exit /b
+if %py.flags.blind% GTR 0 (
+    call ui_io.cmd :printMessage "You can't see to read the scroll."
+    exit /b 1
+)
+
+call player.cmd :playerNoLight && (
+    call ui_io.cmd :printMessage "You have no light to read by."
+    exit /b 1
+)
+
+if %py.flags.confused% GTR 0 (
+    call ui_io.cmd :printMessage "You are too confused to read a scroll."
+    exit /b 1
+)
+
+if "%py.pack.unique_items%"=="0" (
+    call ui_io.cmd :printMessage "You are not carrying anything."
+    exit /b 1
+)
+
+call inventory.cmd :inventoryFindRange %TV_SCROLL1% %TV_SCROLL2% "%~1" "%~2" || (
+    call ui_io.cmd :printMessage "You are not carrying any scrolls."
+    exit /b 1
+)
+exit /b 0
 
 :inventoryItemIdOfCursedEquipment
 exit /b
