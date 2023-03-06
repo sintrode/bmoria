e61460d0c9a4c5125622edf50b057d457c7df108
sintrode <562c3fb8@opayq.com>
2023-03-06 08:49:30 -0500

    Enabled displaying scores

--- src/sys/scores.cmd
+++ src/sys/scores.cmd
@@ -93,6 +93,7 @@ for %%A in ("%config.files.scores%") do (
 call game_save.cmd :setFileptr "%config.files.scores%.hex"
 set "i=0"
 set "curpos=0"
+set "current_byte=0"
 call game_save.cmd :readHighScore "old_entry"
 
 :recordNewHighScoreWhileLoop
@@ -116,14 +117,107 @@ if "!current_byte!"=="!total_bytes!" (
     )
 
     call :recordNewHighScoreSecondWhileLoop
+    call game_save.cmd :saveHighScore "entry"
 )
 exit /b
 
+:: TODO: Confirm that this isn't just some convoluted way of putting the
+:: newest score at the end
 :recordNewHighScoreSecondWhileLoop
-:: TODO: implement later
-exit /b
+if "!current_byte!"=="!total_bytes!" exit /b
+call game_save.cmd :saveHighScore "entry"
+for %%A in (points birth_date uid mhp chp dungeon_depth level
+            deepest_dungeon_depth gender race character_class
+            name died_from) do (
+    set "entry.%%~A=!old_entry.%%~A!"
+)
+call game_save.cmd :readHighScore "old_entry"
+goto :recordNewHighScoreSecondWhileLoop
 
+::------------------------------------------------------------------------------
+:: Display the game's high scores
+::
+:: Arguments: None
+:: Returns:   None
+::------------------------------------------------------------------------------
 :showScoresScreen
+certutil -f -encodehex "%config.files.scores%" "%config.files.scores%.hex" >nul 2>&1
+if not exist "%config.files.scores%.hex" (
+    call :ui_io.cmd :printMessage "Error opening score file '%config.files.scores%'."
+    call :ui_io.cmd :printMessage "CNIL"
+    exit /b
+)
+
+<"%config.files.scores%.hex" set /p "first_score_hex_line="
+for /f "tokens=2-4" %%A in ("%first_score_hex_line%") do (
+    set "version_maj=%%~A"
+    set "version_min=%%~B"
+    set "patch_level=%%~C"
+)
+call game.cmd :validGameVersion "%version_maj%" "%version_min%" "%patch_level%"
+if "!errorlevel!"=="1" (
+    call ui_io.cmd :printMessage "Sorry, this score files is from a different version of bMoria."
+    call ui_io.cmd :printMessage "CNIL"
+    exit /b
+)
+
+call game_save.cmd :setFileptr "%config.files.scores%.hex"
+set "current_byte=0"
+call game_save.cmd :readHighScore "score"
+set "rank=1"
+
+:showScoresScreenOuterWhile
+if "!current_byte!"=="!total_bytes!" exit /b
+set "i=1"
+call ui_io.cmd :clearScreen
+:showScoresScreenInnerWhile
+if not "!current_byte!"=="!total_bytes!" (
+    if !i! LSS 21 (
+        set "msg="
+        call :sprintf msg "!rank!" -4
+        call :sprintf msg "!score.points!" 8
+        call :sprintf msg "!score.name!" -19
+        call :sprintf msg "!score.gender!" 1
+        for /f "tokens=1,2" %%A in ("!score.race! !score.character_class!") do (
+            call :sprintf msg "!character_races[%%~A].name!" -10
+            call :sprintf msg "!classes[%%~B].title!" -7
+        )
+        call :sprintf msg "!score.level!" 3
+        call :sprintf msg "!score.died_from!" -22
+    ) else (
+        goto :showScoresScreenAfterInnerWhile
+    )
+) else (
+    goto :showScoresScreenAfterInnerWhile
+)
+set /a i+=1
+call ui_io.cmd :putStringClearToEOL "!msg!" "!i!;0"
+set /a rank+=1
+call game_save.cmd :readHighScore "score"
+goto :showScoresScreenInnerWhile
+
+:showScoresScreenAfterInnerWhile
+call ui_io.cmd :putStringClearToEOL "Rank  Points Name              Sex Race       Class  Lvl Killed By" "0;0"
+call ui_io.cmd :eraseLine "1;0"
+call ui_io.cmd :putStringClearToEOL "[ press any key to continue ]" "23;23"
+call ui_io.cmd :getKeyInput
+if "!key!"=="Q" exit /b
+goto :showScoresScreenOuterWhile
+
+:sprintf
+set "disp=%~2"
+if %~3 LSS 0 (
+    for /L %%A in (%~3,-1,-1) do (
+        set "disp=!disp! "
+    )
+    set "disp=!disp:~0,%~3!"
+) else (
+    for /L %%A in (1,1,%~3) do (
+        set "disp= !disp!"
+    )
+    set "disp=!disp:~-%~3!"
+)
+set "%~1=!%~1!!disp!"
 exit /b
 
 ::------------------------------------------------------------------------------
