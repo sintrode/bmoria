72d4a8d871c2ba708796c13cea4cfbee98147bf1
sintrode <562c3fb8@opayq.com>
2023-03-27 17:31:08 -0400

    New store inventory

--- src/sys/store_inventory.cmd
+++ src/sys/store_inventory.cmd
@@ -2,7 +2,50 @@
 call %*
 exit /b
 
+::------------------------------------------------------------------------------
+:: Initialize and upkeep the store's inventory
+::
+:: Arguments: None
+:: Returns:   None
+::------------------------------------------------------------------------------
 :storeMaintenance
+set /a store_dec=%MAX_STORES%-1
+for /L %%A in (0,1,%store_dec%) do (
+    set "store=stores[%%A]"
+
+    set "%store%.insults_counter=0"
+    if !%store%.unique_items_counter! GEQ %config.stores.STORE_MIN_AUTO_SELL_ITEMS% (
+        call rng.cmd :randomNumber  %config.stores.STORE_STOCK_TURN_AROUND%
+        set "turnaround=!errorlevel!"
+
+        if !%store%.unique_items_counter! GEQ %config.stores.STORE_MAX_AUTO_SELL_ITEMS% (
+            set /a turnaround+=1 + !%store%.unique_items_counter! - %config.stores.STORE_MAX_AUTO_SELL_ITEMS%
+        )
+        set /a turnaround-=1
+
+        for /L %%B in (!turnaround!,-1,0) do (
+            call rng.cmd :randomNumber !%store%.unique_items_counter!
+            set /a rnd_dec=!errorlevel!-1
+            call :storeDestroyItem "%%A" "!rnd_dec!" "false"
+        )
+        set "turnaround=0"
+    )
+
+    if !%store%.unique_items_counter! LEQ %config.stores.STORE_MAX_AUTO_SELL_ITEMS% (
+        call rng.cmd :randomNumber %config.stores.STORE_STOCK_TURN_AROUND%
+        set "turnaround=!errorlevel!"
+        if !%store%.unique_items_counter! LSS %config.stores.STORE_MIN_AUTO_SELL_ITEMS% (
+            set /a turnaround+=%config.stores.STORE_MIN_AUTO_SELL_ITEMS% - !%store%.unique_items_counter!
+        )
+
+        for /f "delims=" %%B in ("!%store%.owner_id!") do set "max_cost=!store_owners[%%~B].max_cost!"
+
+        set /a turnaround-=1
+        for /L %%B in (!turnaround!,-1,0) do (
+            call :storeItemCreate "%%A" "!max_cost!"
+        )
+    )
+)
 exit /b
 
 :storeItemValue
