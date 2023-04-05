2c9b470135e82776c0f1303f39afbd23caaece70
sintrode <562c3fb8@opayq.com>
2023-04-05 14:04:47 -0400

    Calculate magical enchantment bonus

--- src/sys/treasure.cmd
+++ src/sys/treasure.cmd
@@ -14,8 +14,27 @@ call rng.cmd :randomNumber 100
 if !errorlevel! LEQ %~1 exit /b 0
 exit /b 1
 
+::------------------------------------------------------------------------------
+:: Enchant a bonus based on a desired amount
+::
+:: Arguments: %1 - The base value of the bonus
+::            %2 - The maximum value from one standard_deviation
+::            %3 - The level of the dungeon that the player is on
+:: Returns:   The new bonus for the item
+::------------------------------------------------------------------------------
 :magicEnchantmentBonus
-exit /b
+set /a standard_deviation=(%config.treasure.LEVEL_STD_OBJECT_ADJUST% * %~3 / 100) + %config.treasure.LEVEL_MIN_OBJECT_STD%
+
+if %standard_deviation% GTR %~2 set "standard_deviation=%~2"
+if %~3 GTR %~2 set "standard_deviation=%~2"
+
+call game.cmd :randomNumberNormalDistribution 0 %standard_deviation%
+set "abs_distribution=!errorlevel!"
+if !abs_distribution! LSS 0 set /a abs_distribution*=-1
+set /a bonus=(!abs_distribution! / 10) + %~1
+
+if !bonus! LSS %~1 exit /b %~1
+exit /b !bonus!
 
 :magicalArmor
 exit /b
