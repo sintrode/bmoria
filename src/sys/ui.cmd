5f8790eb5eb4143e4d2015573e0405e00fb2a9e8
sintrode <562c3fb8@opayq.com>
2023-04-10 03:10:24 -0400

    Draw the screen

--- src/sys/ui.cmd
+++ src/sys/ui.cmd
@@ -102,7 +102,17 @@ for /f "tokens=1,2 delims=;" %%A in ("%~1") do (
 set /a "is_valid=!valid_y! | !valid_x!"
 exit /b !is_valid!
 
+::------------------------------------------------------------------------------
+:: Draw the entire screen
+::
+:: Arguments: None
+:: Returns:   None
+::------------------------------------------------------------------------------
 :drawDungeonPanel
+call ui_io.cmd :clearScreen
+call :printCharacterStatsBlock
+call :drawDungeonPanel
+call :printCharacterCurrentDepth
 exit /b
 
 :drawCavePanel
