1ecfbf15087f4292c5e58333ecbc79d6369edea1
sintrode <562c3fb8@opayq.com>
2023-04-11 00:55:55 -0400

    Prompt for going Wizard Mode

--- src/sys/wizard.cmd
+++ src/sys/wizard.cmd
@@ -2,8 +2,31 @@
 call %*
 exit /b
 
+::------------------------------------------------------------------------------
+:: Display a disclaimer and enter Wizard Mode
+::
+:: Arguments: None
+:: Returns:   0 if the player agrees to enter Wizard Mode
+::            1 if the player wants to play a scored game instead
+::------------------------------------------------------------------------------
 :enterWizardMode
-exit /b
+set "answer=1"
+
+if "%game.noscore%"=="0" (
+    call ui_io.cmd :printMessage "Wizard mode is for debugging and experimenting."
+    call ui_io.cmd :getInputConfirmation "The game will not be scored if you enter Wizard Mode. Are you sure?"
+    set "answer=!errorlevel!"
+)
+
+set "become_wizard=0"
+if not "%game.noscore%"=="0" set "become_wizard=1"
+if "!answer!"=="0" set "become_wizard=1"
+if "!become_wizard!"=="1" (
+    set /a "game.noscore|=2"
+    set "game.wizard_mode=true"
+    exit /b 0
+)
+exit /b 1
 
 :wizardCureAll
 exit /b
